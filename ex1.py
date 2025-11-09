5db4a703cf6e01222410e4a800fb14bf52cc4143
Angeline <angelineyzh@gmail.com>
2025-11-09 12:12:58 +0000

    submission

--- ex1.py
+++ ex1.py
@@ -1,3 +1,13 @@
-def find(s, n):
-# write your implementation here
-    return None
+def find(numList, target):
+    index = {} #dict to store {num: index} of the numbers we have checked
+
+    for i, num in enumerate(numList): #enumerate(numList) - loop over a list and get both the index and the value at the same time.
+        difference = target - num
+        if difference in index:
+            return [index[difference], i]
+        else:
+            index[num] = i 
+
+print(find([2,7,11,15], 9)) 
+
+
